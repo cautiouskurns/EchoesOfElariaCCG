54b3cb0344ec4002fd36819578340a342061a9a9
cautiouskurns <114108720+cautiouskurns@users.noreply.github.com>
2025-02-07 12:40:31 +0000

    VCTest

---
+++ Assets/5. Scripts/UI/VCTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class VCTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("[VCTest] 🚀 Starting Version Control Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
